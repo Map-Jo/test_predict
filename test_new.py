80f12e80e39784314c5f4f06fe12ae17739a7e7f
Map-Jo <ahyun1609@likelion.org>
2022-09-22 14:49:08 +0900

    Update test_new.py

--- test_new.py
+++ test_new.py
@@ -14,8 +14,12 @@ if Name in Code_name_list:
     code_num = Stockcode.at[Name, 'Symbol']
     code_num = code_num + '.KS'
     # st.text(code_num)
-    stock_df = pdr.get_data_yahoo(code_num)
+    df = pdr.get_data_yahoo(code_num)
     # stock_df
+    col1, col2, col3 = st.columns(3)
+    col1.metric("현재 주식가격",format(df['Close'].tail(1)[0], ',')+'원', "%d원" %(df['Close'].diff().tail(1)[0]))
+    col2.metric("현재 거래량", format(df['Volume'].tail(1)[0], ','),"%.2f%%" %(df['Volume'].pct_change().tail(1)[0] * 100))
+    col3.metric("전일 대비 가격", "%d원" %(df['Close'].diff().tail(1)[0]), "%.2f%%" %(df['Change'].tail(1)[0] * 100))
 
 elif Name not in Code_name_list:
     st.text('검색하신 주식 종목이 없습니다. 정확하게 입력해주세요.')
